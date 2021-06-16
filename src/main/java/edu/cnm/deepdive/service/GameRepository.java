f734ab79141a6673e0c6271d4f4483fd8db91407
Marshall Eldridge <l2marshall@gmail.com>
2021-06-16 10:28:58 -0600

    Started implementing newGame method.

---
+++ src/main/java/edu/cnm/deepdive/service/GameRepository.java
@@ -0,0 +1,27 @@
+package edu.cnm.deepdive.service;
+
+import edu.cnm.deepdive.model.Game;
+import java.io.IOException;
+import retrofit2.Response;
+
+public class GameRepository {
+
+  private final CodebreakerServiceProxy proxy;
+
+
+  public GameRepository() {
+    proxy = CodebreakerServiceProxy.getInstance();
+  }
+
+  public Game newGame(String pool, int length) throws IOException {
+    Game game = new Game();
+    game.setPool(pool);
+    game.setLength(length);
+    Response<Game> response = proxy.startGame(game).execute();
+    if (!response.isSuccessful()) {
+      throw new IllegalArgumentException();
+    }
+    return response.body();
+
+  }
+}
